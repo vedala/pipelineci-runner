4bdcaeb2a1328bc45536914d7811bd9f83db7793
Kalyan Vedala <svedala23@gmail.com>
2024-12-19 12:55:07 -0800

    Convert back to express app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { App } from "octokit";
 import { Octokit } from "@octokit/core";
+import express from "express";
 import * as tar from 'tar';
 import { writeFile } from "fs/promises";
 import { Readable } from "stream";
@@ -8,10 +9,14 @@ import { exec } from "child_process";
 
 dotenv.config();
 
+const app = express();
+
+const port = process.env.PORT;
 const appId = process.env.GITHUB_APP_IDENTIFIER;
 const webhookSecret = process.env.WEBHOOK_SECRET;
 const privateKey = process.env.GITHUB_APP_PRIVATE_KEY;
 
+app.use(express.json());
 // const ghApp = new App({
 //   appId: appId,
 //   privateKey: privateKey,
@@ -24,84 +29,87 @@ const privateKey = process.env.GITHUB_APP_PRIVATE_KEY;
 //   return new Promise(resolve => setTimeout(resolve, ms));
 // }
 
-console.log("POST /run_ci called");
+app.post("/run_ci", async (req, res) => {
 
-const installationToken = process.env.INSTALLATION_TOKEN;
-// const eventPayload = req.body.payload;
-const repoOwner = process.env.REPO_OWNER;
-const repoToClone = process.env.REPO_NAME;
-const branch = process.env.REPO_BRANCH;
 
-const octokitClient = new Octokit({
-  auth: installationToken
-});
+  console.log("POST /run_ci called");
 
-const ghAppResponse = await octokitClient.request("GET /repos/{owner}/{repo}/tarball/{ref}", {
-  owner: repoOwner,
-  repo: repoToClone,
-  ref: branch,
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-});
+  const installationId = req.body.installationId;
+  const repoOwner = req.body.repoOwner;
+  const repoToClone = req.body.repoToClone;
+  const branch = req.body.branch;
 
-console.log("ghAppResponse=", ghAppResponse);
+  const octokitClient = new Octokit({
+    auth: installationToken
+  });
 
-const headerContentDisposition = ghAppResponse.headers['content-disposition'];
-const tarballFileName = headerContentDisposition.replace(/^.*filename=/, "");
-
-const urlToDownload = ghAppResponse.url;
-
-let executionStatus;
-
-console.log("Downloading", urlToDownload);
-try {
-  const downloadResponse = await fetch(urlToDownload);
-  const downloadBody = Readable.fromWeb(downloadResponse.body);
-  await writeFile(tarballFileName, downloadBody);
-  console.log("Download done");
-
-  await tar.extract(
-    {
-      f: tarballFileName
+  const ghAppResponse = await octokitClient.request("GET /repos/{owner}/{repo}/tarball/{ref}", {
+    owner: repoOwner,
+    repo: repoToClone,
+    ref: branch,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
     }
-  ).then( _ => { console.log("tarball has been dumped in cwd") })
+  });
 
-  const filenameNoExtension = tarballFileName.replace(/.tar.gz$/, "");
+  console.log("ghAppResponse=", ghAppResponse);
+
+  const headerContentDisposition = ghAppResponse.headers['content-disposition'];
+  const tarballFileName = headerContentDisposition.replace(/^.*filename=/, "");
+
+  const urlToDownload = ghAppResponse.url;
+
+  let executionStatus;
+
+  console.log("Downloading", urlToDownload);
   try {
-    exec(`cd ${filenameNoExtension}; ./pipelineci.sh`);
-    console.log("./pipelineci.sh executed successfully.")
-  } catch(e) {
-    console.log("./pipelineci.sh execution failed.")
-    console.log("error=", e.message);
+    const downloadResponse = await fetch(urlToDownload);
+    const downloadBody = Readable.fromWeb(downloadResponse.body);
+    await writeFile(tarballFileName, downloadBody);
+    console.log("Download done");
+
+    await tar.extract(
+      {
+        f: tarballFileName
+      }
+    ).then( _ => { console.log("tarball has been dumped in cwd") })
+
+    const filenameNoExtension = tarballFileName.replace(/.tar.gz$/, "");
+    try {
+      exec(`cd ${filenameNoExtension}; ./pipelineci.sh`);
+      console.log("./pipelineci.sh executed successfully.")
+    } catch(e) {
+      console.log("./pipelineci.sh execution failed.")
+      console.log("error=", e.message);
+    }
+
+    executionStatus = "success";
+
+    // console.log("Sleeping for 20 seconds");
+    // await sleep(20000);
+    // console.log("Sleeping done");
+
+    // await octokitClient.request("POST /repos/{owner}/{repo}/statuses/{sha}", {
+    //   owner: repoOwner,
+    //   repo: repoToClone,
+    //   sha: eventPayload.pull_request.head.sha,
+    //   state: "success",
+    //   target_url: 'https://example.com/build/status',
+    //   description: 'Description from app.js',
+    //   context: 'ci-update/status-update',
+    //   headers: {
+    //     "x-github-api-version": "2022-11-28",
+    //   },
+    // });
+
+  } catch (e) {
+    console.log("Download failed");
+    console.log(e.message);
+    executionStatus = "failure"
   }
 
-  executionStatus = "success";
+  console.log("Execution status: ", executionStatus);
 
-  // console.log("Sleeping for 20 seconds");
-  // await sleep(20000);
-  // console.log("Sleeping done");
+  // res.send("CI checks successful.");
 
-  // await octokitClient.request("POST /repos/{owner}/{repo}/statuses/{sha}", {
-  //   owner: repoOwner,
-  //   repo: repoToClone,
-  //   sha: eventPayload.pull_request.head.sha,
-  //   state: "success",
-  //   target_url: 'https://example.com/build/status',
-  //   description: 'Description from app.js',
-  //   context: 'ci-update/status-update',
-  //   headers: {
-  //     "x-github-api-version": "2022-11-28",
-  //   },
-  // });
-
-} catch (e) {
-  console.log("Download failed");
-  console.log(e.message);
-  executionStatus = "failure"
-}
-
-console.log("Execution status: ", executionStatus);
-
-// res.send("CI checks successful.");
-
+});
